40cabdb57cf9f98e301947941929ee72100d38cf
W E D Y <110571114+WalterDelgado1@users.noreply.github.com>
2023-05-30 16:13:17 -0500

    Repository - Recomendacion

--- src/main/java/pe/edu/upc/pcinfo/repositories/IRecomendacionRepository.java
+++ src/main/java/pe/edu/upc/pcinfo/repositories/IRecomendacionRepository.java
@@ -1,4 +1,9 @@
 package pe.edu.upc.pcinfo.repositories;
 
-public interface IRecomendacionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.pcinfo.entities.Recomendacion;
+
+@Repository
+public interface IRecomendacionRepository extends JpaRepository<Recomendacion,Integer> {
 }
