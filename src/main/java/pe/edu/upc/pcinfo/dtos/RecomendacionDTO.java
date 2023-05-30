f3ca16175e6171c8fe9baed99c6f7dab41c17372
W E D Y <110571114+WalterDelgado1@users.noreply.github.com>
2023-05-30 16:18:21 -0500

    DTO recomendacion

--- src/main/java/pe/edu/upc/pcinfo/dtos/RecomendacionDTO.java
+++ src/main/java/pe/edu/upc/pcinfo/dtos/RecomendacionDTO.java
@@ -1,4 +1,34 @@
 package pe.edu.upc.pcinfo.dtos;
 
+import javax.persistence.Column;
+
 public class RecomendacionDTO {
+
+    private int id;
+    private int nmRecomend;
+    private String msnRecomnd;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNmRecomend() {
+        return nmRecomend;
+    }
+
+    public void setNmRecomend(int nmRecomend) {
+        this.nmRecomend = nmRecomend;
+    }
+
+    public String getMsnRecomnd() {
+        return msnRecomnd;
+    }
+
+    public void setMsnRecomnd(String msnRecomnd) {
+        this.msnRecomnd = msnRecomnd;
+    }
 }
