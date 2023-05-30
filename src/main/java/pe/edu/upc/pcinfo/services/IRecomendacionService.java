6c374ea50e165db579871f8eebf4424b1588dccd
W E D Y <110571114+WalterDelgado1@users.noreply.github.com>
2023-05-30 16:14:32 -0500

    Service Recomendacion

--- src/main/java/pe/edu/upc/pcinfo/services/IRecomendacionService.java
+++ src/main/java/pe/edu/upc/pcinfo/services/IRecomendacionService.java
@@ -1,4 +1,13 @@
 package pe.edu.upc.pcinfo.services;
 
+import pe.edu.upc.pcinfo.entities.Recomendacion;
+
+import java.util.List;
+
 public interface IRecomendacionService {
+
+    public void insert(Recomendacion recomendacion);
+    List<Recomendacion> list();
+    public void delete(int id);
+    public Recomendacion listId(int id);
 }
