f0d5a24fe02705d5a5edcb1d9a59b9e6673ef835
W E D Y <110571114+WalterDelgado1@users.noreply.github.com>
2023-05-30 16:16:46 -0500

    Service Implement Recomendacion

--- src/main/java/pe/edu/upc/pcinfo/servicesimplement/RecomendacionServiceImplement.java
+++ src/main/java/pe/edu/upc/pcinfo/servicesimplement/RecomendacionServiceImplement.java
@@ -1,4 +1,36 @@
 package pe.edu.upc.pcinfo.servicesimplement;
 
-public class RecomendacionServiceImplement {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.pcinfo.entities.Recomendacion;
+import pe.edu.upc.pcinfo.repositories.IRecomendacionRepository;
+import pe.edu.upc.pcinfo.services.IRecomendacionService;
+
+import java.util.List;
+
+@Service
+public class RecomendacionServiceImplement implements IRecomendacionService {
+
+    @Autowired
+    private IRecomendacionRepository rR;
+    @Override
+    public void insert(Recomendacion recomendacion) {
+        rR.save(recomendacion);
+    }
+
+    @Override
+    public List<Recomendacion> list() {
+        return rR.findAll();
+    }
+
+    @Override
+    public void delete(int id) {
+        rR.deleteById(id);
+    }
+
+    @Override
+    public Recomendacion listId(int id) {
+        return rR.findById(id).orElse(new Recomendacion());
+    }
 }
