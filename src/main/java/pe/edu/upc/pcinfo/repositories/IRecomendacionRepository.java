f1fcbc70a24fddb2a43edc00e0d1ce178116fcb6
W E D Y <110571114+WalterDelgado1@users.noreply.github.com>
2023-05-30 16:11:01 -0500

    Creación_Partes_Recomendacion

---
+++ src/main/java/pe/edu/upc/pcinfo/repositories/IRecomendacionRepository.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.pcinfo.repositories;
+
+public interface IRecomendacionRepository {
+}
---
+++ src/main/java/pe/edu/upc/pcinfo/services/IRecomendacionService.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.pcinfo.services;
+
+public interface IRecomendacionService {
+}
