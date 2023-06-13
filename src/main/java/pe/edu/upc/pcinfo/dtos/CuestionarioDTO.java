6eb50c811a842165bfe10e38662b962a0973860b
Fabianick <fabianick2014@gmail.com>
2023-06-13 14:52:11 -0500

    Conexion de cuestionario

--- src/main/java/pe/edu/upc/pcinfo/dtos/CuestionarioDTO.java
+++ src/main/java/pe/edu/upc/pcinfo/dtos/CuestionarioDTO.java
@@ -5,14 +5,14 @@ import pe.edu.upc.pcinfo.entities.Cliente;
 public class CuestionarioDTO {
     private int id;
     private String tipo_form;
-    private String Uso_de_pc;
-    private String Presupuesto;
-    private String Tamano_del_PC;
-    private String Diseno;
-    private String Tipo_de_refrigeracion;
-    private boolean Overclocking;
-    private String Marca_Procesador;
-    private String Programas_used;
+    private String uso_de_pc;
+    private String presupuesto;
+    private String tamano_del_pc;
+    private String diseno;
+    private String tipo_de_refrigeracion;
+    private String overclocking;
+    private String marca_procesador;
+    private String programas_used;
 
     private Cliente cliente;
 
@@ -33,67 +33,67 @@ public class CuestionarioDTO {
     }
 
     public String getUso_de_pc() {
-        return Uso_de_pc;
+        return uso_de_pc;
     }
 
     public void setUso_de_pc(String uso_de_pc) {
-        Uso_de_pc = uso_de_pc;
+        this.uso_de_pc = uso_de_pc;
     }
 
     public String getPresupuesto() {
-        return Presupuesto;
+        return presupuesto;
     }
 
     public void setPresupuesto(String presupuesto) {
-        Presupuesto = presupuesto;
+        this.presupuesto = presupuesto;
     }
 
-    public String getTamano_del_PC() {
-        return Tamano_del_PC;
+    public String getTamano_del_pc() {
+        return tamano_del_pc;
     }
 
-    public void setTamano_del_PC(String tamano_del_PC) {
-        Tamano_del_PC = tamano_del_PC;
+    public void setTamano_del_pc(String tamano_del_pc) {
+        this.tamano_del_pc = tamano_del_pc;
     }
 
     public String getDiseno() {
-        return Diseno;
+        return diseno;
     }
 
     public void setDiseno(String diseno) {
-        Diseno = diseno;
+        this.diseno = diseno;
     }
 
     public String getTipo_de_refrigeracion() {
-        return Tipo_de_refrigeracion;
+        return tipo_de_refrigeracion;
     }
 
     public void setTipo_de_refrigeracion(String tipo_de_refrigeracion) {
-        Tipo_de_refrigeracion = tipo_de_refrigeracion;
+        this.tipo_de_refrigeracion = tipo_de_refrigeracion;
     }
 
-    public boolean isOverclocking() {
-        return Overclocking;
+    public String getOverclocking() {
+        return overclocking;
     }
 
-    public void setOverclocking(boolean overclocking) {
-        Overclocking = overclocking;
+    public void setOverclocking(String overclocking) {
+        this.overclocking = overclocking;
     }
 
-    public String getMarca_Procesador() {
-        return Marca_Procesador;
+    public String getMarca_procesador() {
+        return marca_procesador;
     }
 
-    public void setMarca_Procesador(String marca_Procesador) {
-        Marca_Procesador = marca_Procesador;
+    public void setMarca_procesador(String marca_procesador) {
+        this.marca_procesador = marca_procesador;
     }
 
     public String getProgramas_used() {
-        return Programas_used;
+        return programas_used;
     }
 
     public void setProgramas_used(String programas_used) {
-        Programas_used = programas_used;
+        this.programas_used = programas_used;
     }
 
     public Cliente getCliente() {
--- src/main/java/pe/edu/upc/pcinfo/entities/Cuestionario.java
+++ src/main/java/pe/edu/upc/pcinfo/entities/Cuestionario.java
@@ -11,21 +11,21 @@ public class Cuestionario {
     @Column(name = "tipo_form",length = 60, nullable = false)
     private String tipo_form;
     @Column(name = "uso_de_pc",length = 60, nullable = false)
-    private String Uso_de_pc;
+    private String uso_de_pc;
     @Column(name = "presupuesto",length = 60, nullable = false)
-    private String Presupuesto;
+    private String presupuesto;
     @Column(name = "tamano_del_pc",length = 60, nullable = false)
-    private String Tamano_del_PC;
+    private String tamano_del_pc;
     @Column(name = "diseno",length = 60, nullable = false)
-    private String Diseno;
+    private String diseno;
     @Column(name = "tipo_de_refrigeracion",length = 60, nullable = false)
-    private String Tipo_de_refrigeracion;
-    @Column(name = "overclocking")
-    private boolean Overclocking;
+    private String tipo_de_refrigeracion;
+    @Column(name = "overclocking",length = 60, nullable = false)
+    private String overclocking;
     @Column(name = "marca_procesador",length = 60, nullable = false)
-    private String Marca_Procesador;
+    private String marca_procesador;
     @Column(name = "programas_used",length = 60, nullable = false)
-    private String Programas_used;
+    private String programas_used;
 
     @ManyToOne
     @JoinColumn(name = "idCliente")
@@ -34,17 +34,17 @@ public class Cuestionario {
     public Cuestionario() {
     }
 
-    public Cuestionario(int id, String tipo_form, String uso_de_pc, String presupuesto, String tamano_del_PC, String diseno, String tipo_de_refrigeracion, boolean overclocking, String marca_Procesador, String programas_used, Cliente cliente) {
+    public Cuestionario(int id, String tipo_form, String uso_de_pc, String presupuesto, String tamano_del_pc, String diseno, String tipo_de_refrigeracion, String overclocking, String marca_procesador, String programas_used, Cliente cliente) {
         this.id = id;
         this.tipo_form = tipo_form;
-        this.Uso_de_pc = uso_de_pc;
-        this.Presupuesto = presupuesto;
-        this.Tamano_del_PC = tamano_del_PC;
-        this.Diseno = diseno;
-        this.Tipo_de_refrigeracion = tipo_de_refrigeracion;
-        this.Overclocking = overclocking;
-        this.Marca_Procesador = marca_Procesador;
-        this.Programas_used = programas_used;
+        this.uso_de_pc = uso_de_pc;
+        this.presupuesto = presupuesto;
+        this.tamano_del_pc = tamano_del_pc;
+        this.diseno = diseno;
+        this.tipo_de_refrigeracion = tipo_de_refrigeracion;
+        this.overclocking = overclocking;
+        this.marca_procesador = marca_procesador;
+        this.programas_used = programas_used;
         this.cliente = cliente;
     }
 
@@ -65,67 +65,67 @@ public class Cuestionario {
     }
 
     public String getUso_de_pc() {
-        return Uso_de_pc;
+        return uso_de_pc;
     }
 
     public void setUso_de_pc(String uso_de_pc) {
-        Uso_de_pc = uso_de_pc;
+        this.uso_de_pc = uso_de_pc;
     }
 
     public String getPresupuesto() {
-        return Presupuesto;
+        return presupuesto;
     }
 
     public void setPresupuesto(String presupuesto) {
-        Presupuesto = presupuesto;
+        this.presupuesto = presupuesto;
     }
 
-    public String getTamano_del_PC() {
-        return Tamano_del_PC;
+    public String getTamano_del_pc() {
+        return tamano_del_pc;
     }
 
-    public void setTamano_del_PC(String tamano_del_pc) {
-        Tamano_del_PC = tamano_del_pc;
+    public void setTamano_del_pc(String tamano_del_pc) {
+        this.tamano_del_pc = tamano_del_pc;
     }
 
     public String getDiseno() {
-        return Diseno;
+        return diseno;
     }
 
     public void setDiseno(String diseno) {
-        Diseno = diseno;
+        this.diseno = diseno;
     }
 
     public String getTipo_de_refrigeracion() {
-        return Tipo_de_refrigeracion;
+        return tipo_de_refrigeracion;
     }
 
     public void setTipo_de_refrigeracion(String tipo_de_refrigeracion) {
-        Tipo_de_refrigeracion = tipo_de_refrigeracion;
+        this.tipo_de_refrigeracion = tipo_de_refrigeracion;
     }
 
-    public boolean isOverclocking() {
-        return Overclocking;
+    public String getOverclocking() {
+        return overclocking;
     }
 
-    public void setOverclocking(boolean overclocking) {
-        Overclocking = overclocking;
+    public void setOverclocking(String overclocking) {
+        this.overclocking = overclocking;
     }
 
-    public String getMarca_Procesador() {
-        return Marca_Procesador;
+    public String getMarca_procesador() {
+        return marca_procesador;
     }
 
-    public void setMarca_Procesador(String marca_Procesador) {
-        Marca_Procesador = marca_Procesador;
+    public void setMarca_procesador(String marca_procesador) {
+        this.marca_procesador = marca_procesador;
     }
 
     public String getProgramas_used() {
-        return Programas_used;
+        return programas_used;
     }
 
     public void setProgramas_used(String programas_used) {
-        Programas_used = programas_used;
+        this.programas_used = programas_used;
     }
 
     public Cliente getCliente() {
